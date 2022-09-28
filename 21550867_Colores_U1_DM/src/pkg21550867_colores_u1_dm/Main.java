11a510e014653929046c712d47edaa549893034f
Sarahi Rivera <99987448+Sarahi13Rivera@users.noreply.github.com>
2022-09-27 20:38:02 -0600

    Add files via upload

---
+++ 21550867_Colores_U1_DM/src/pkg21550867_colores_u1_dm/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg21550867_colores_u1_dm;
+
+/**
+ *
+ * @author HP1000
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          pantallaPricipal n = new pantallaPricipal ();
+        n.setVisible(true);
+    }
+    
+}
