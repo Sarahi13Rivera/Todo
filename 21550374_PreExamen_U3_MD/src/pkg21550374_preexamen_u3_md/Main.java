d1a00e21f1586ff830c1ff3a26db74d46fc38ee2
Sarahi Rivera <99987448+Sarahi13Rivera@users.noreply.github.com>
2022-09-27 20:31:19 -0600

    Add files via upload

---
+++ 21550374_PreExamen_U3_MD/src/pkg21550374_preexamen_u3_md/Main.java
@@ -0,0 +1,10 @@
+package pkg21550374_preexamen_u3_md;
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu pObj= new Menu(); 
+        pObj.setVisible(true);
+    }
+    
+}
